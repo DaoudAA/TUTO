e6338d5208d235f8bedafb7a83d94b1ef33f93f6
Ahm3da <Ahmedaziz.daoud@gmail.com>
2023-04-15 01:27:08 +0100

    ostream out of class

--- PrixJournalier.h
+++ PrixJournalier.h
@@ -10,21 +10,20 @@ class PrixJournalier
 {
 private:
     Date  date;
-    string const nomAction;
+    string nomAction;
     double  prix;
 public:
-    PrixJournalier(Date  d, string  n , double  p):date(d),nomAction(n),prix(p){;}
-    friend ostream& operator<<(ostream& flux , const PrixJournalier& pj){
+    PrixJournalier(Date  d, string  n , double  p):date(d),nomAction(n),prix(p){}
+    friend ostream& operator<<(ostream& , const PrixJournalier&);
+
+};
+
+ostream& operator<<(ostream& flux , const PrixJournalier& pj){
         flux<<"La date:\t"<<pj.date<<endl;
         flux<<"Le nom de l'action:\t"<< pj.nomAction<<endl;
         flux<<"Le prix est:\t" << pj.prix <<endl;
         return flux;
     }
-
-};
-
-
-
 
 
 #endif // PRIXJOURNALIER_H_INCLUDED
