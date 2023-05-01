e54dd3ca4ffe7da17ca242588e1d641302cf2ea0
Ahm3da <Ahmedaziz.daoud@gmail.com>
2023-05-01 20:57:39 +0100

    main1 into main

--- main.cpp
+++ main.cpp
@@ -8,30 +8,12 @@ int main(){
     PersistancePrixJournaliers ppj;
 	vPj=ppj.lirePrixJournaliersDUnFichier(ch1);
     Bourse * bourse=new BourseVector2(vPj);
-    //cout << (*bourse).getHistoriqueAction("LVLT")[0]<<endl ; 
-    Trader * trader=new TraderBollin1;
+    Trader * trader=new TraderAlea;
     map <string , long > statiktiks;
     double soldeInit=800;
     Date dateDebut(1,4,2010);
     Date dateFin(7,10,2010);
-    /*do {
-        cout << "Solde Initial : "<< endl; 
-        cin >> soldeInit ;
-    }while (soldeInit<0) ;*/
     Simulation Sim(soldeInit);
-    /*do {
-        cout << "date Debut : "<< endl; 
-        cin >> dateDebut ;
-    }while (dateDebut>Date(31,12,2016));
-    do {
-        cout << "date Fin : "<< endl; 
-        cin >> dateFin ;
-    }while ((dateFin<dateDebut )&&(dateFin<Date(1,1,2010))) ;*/
-    
-    /*for (Date d=dateDebut;d<dateFin;d.incrementerDate()){
-        TestDate test
-        cout << d;
-         }*/
    statiktiks=Sim.executer(*bourse,*trader,dateDebut,dateFin,soldeInit);
     cout<< "Stats : "<<endl;
     for(auto elt : statiktiks){
