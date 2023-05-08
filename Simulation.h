1409f2e6c679608232138edbecb4c2db91ac62dd
rania1231 <raniahamdi3112@gmail.com>
2023-05-08 12:07:39 +0100

    update stats in simulation

--- Simulation.h
+++ Simulation.h
@@ -34,7 +34,7 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 	{   
 		//dans une meme journee
 		//stats pour la fct getPrixJournalierAujourdhui
-		stats["getPrixJournaliersAujourdhui"]++;
+
 		auto start = chrono::high_resolution_clock::now();
 		vector<PrixJournalier> Pj=bourse.getPrixJournaliersAujourdhui();
 		auto stop = chrono::high_resolution_clock::now();
@@ -42,14 +42,14 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 		stats["Temps_GetPrixJournalierAujourdhui_µs"]+=duration.count();
 		//stats pour la fct getActionDisponibleAujourdhui
 		start = chrono::high_resolution_clock::now();
-		stats["getActionDisponibleAujourdhui"]++;
 		vector<string> Actions=bourse.getActionDisponibleAujourdhui();
 		stop = chrono::high_resolution_clock::now();
 		duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 		stats["Temps_GetActionDisponibleAujourdhui_µs"]+=duration.count();
 		// les choix de transactions dans une meme journee
 		int i=0;
-		while(i<10){	
+		while(i<100)
+		{	
 			//stats pour la fct choisirTransaction
 		    stats["NombreDeTransaction"]++;
 		    start = chrono::high_resolution_clock::now();
@@ -66,12 +66,12 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 					break;
 				}
 				else if ((T.getTypeTx()==achat)&&(T.getqtedAction()>0)){
-					stats["nombreDAchat"]++;
+					
 					//bool found = appartientAction(actionNom,Actions);
 					double p=bourse.getPrixAujourdhui(T.getnomdAction(),Actions);
 				    if ((portefeuille.getSolde()>=p)&& (p>0.5)) {
+						stats["nombreDAchat"]++;
 						cout <<portefeuille.getSolde() <<"\t" << p << endl; 
-						stats["getPrixAujourdhui(action)/NbDachatachevees"]++;
 					    action=T.getnomdAction();
 					    qte=T.getqtedAction();
 					    prix=p;
@@ -80,13 +80,14 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 					}
 				}
 				else if ((T.getTypeTx()==vente)&&(T.getqtedAction()>0)){
-					stats["nombreDVente"]++;
+					
 					for(unsigned int i=0;i<(portefeuille.titres).size();i++){
 						if ((portefeuille.titres)[i].getNomAction() == actionNom){
+							stats["nombreDVente"]++;
 							action=T.getnomdAction();
 							qte=T.getqtedAction();
 							prix=bourse.getLastPrixAction(action);
-							stats["getLastPrixAction/NbDventeachevees"]++;
+							stats["NombreDActionsPresentesLorsDuDernierJour"]++;
 							portefeuille.venteTitre(action,qte,prix);
 					cout<<"Vente de "<<T.getqtedAction()<<" of "<<T.getnomdAction()<<endl ; 
 						}
@@ -94,7 +95,7 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 					}
 				}
 				i++;
-				//cout<<"solde du portefeuille"<<portefeuille.getSolde();
+				cout<<"solde du portefeuille"<<portefeuille.getSolde();
 		}
 		(bourse.dateAujourdhui).incrementerDate();
 
@@ -103,20 +104,16 @@ map <string , long > Simulation::executer(Bourse& bourse, Trader& trader, Date d
 	for(unsigned int i=0;i<portefeuille.getTitre().size();i++){
 		if(portefeuille.getTitre()[i].getQte()>0){
 			action=portefeuille.getTitre()[i].getNomAction();
-			//cout<<"nom de laction dans le portefeuille"<<action<<endl;
 			qte=portefeuille.getTitre()[i].getQte();
-			//cout<<"quantite de laction dans le portefeuille"<<qte<<endl;
 			prix=bourse.getLastPrixAction(action);
-			//cout<<"prix de laction dans le portefeuille"<<prix<<endl;
-			stats["getLastPrixAction"]++;
+			stats["NombreDActionsPresentesLorsDuDernierJour"]++;
 			portefeuille.venteTitre(action,qte,prix);
-			stats["nombreDVente"]++;
-			//cout<<"le solde portefeuille"<<portefeuille.getSolde()<<endl;
+			stats["nombreDVentes"]++;
 		}
 	}	
 	}
 	stats["soldeFinal"]=portefeuille.getSolde();
-	stats["Taux_du_gain_en_%"]=stats["soldeFinal"]/soldeInit*100;
+	stats["Taux_du_gain_en_%"]=stats["soldeFinal"]/soldeInit*100-100;
 	return stats;
 
 }
