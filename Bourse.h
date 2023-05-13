e1b567ab103e9248acaf78cddd160189e6cdc90c
Ahm3da <Ahmedaziz.daoud@gmail.com>
2023-05-13 20:24:59 +0100

    bourse finale

--- Bourse.h
+++ Bourse.h
@@ -97,66 +97,42 @@ vector<string> BourseVector::getActionDisponibleParDate(const Date& d) const {
 }
 
 vector<PrixJournalier> BourseVector::getPrixJournaliersParDate( const Date &d)const{
-    cout<<"f1"<<endl;
     vector<PrixJournalier> prixJParDate;
-    cout<<"f2"<<endl;
     if(d<=dateAujourdhui){
-        cout<<"f3"<<endl;
         vector<PrixJournalier> pj=recherchePrixJournalier(historique,d);
-        cout<<"f4"<<endl;
-        for(unsigned int i=0;i<pj.size();i++){
-                cout<<"f5"<<endl;
-                if(!(appartientPrixJournalier(pj[i],prixJParDate)))	
-            	    prixJParDate.push_back(pj[i]);
-        }
+        for(unsigned int i=0;i<pj.size();i++)
+        if(!(appartientPrixJournalier(pj[i],prixJParDate)))	
+            prixJParDate.push_back(pj[i]);
     }
     return prixJParDate;
 }
 
-void rechercheDichotomiqueVector(const Date d,const vector<PrixJournalier> &liste,int &le,int &ri,int &mi){
-cout<<"h1"<<endl;
+void rechercheDichotomiqueVector(const Date d,const vector<PrixJournalier> &liste,int le,int ri,int mi){
 while(le<=ri){
-        cout<<"h2"<<endl;
 		mi=(ri+le)/2;
-        cout<<"h3"<<endl;
         if(d<liste[mi].getDate()){
-            cout<<"h4"<<endl;
             ri=mi-1;
         }
         else if(d>liste[mi].getDate()){
-            cout<<"h5"<<endl;
             le=mi+1;
         }
-        else {
-            cout<<"h6"<<endl;
-            break;
+        else break;
         }
-        cout<<"h7"<<endl;
-    }
 }
-vector<PrixJournalier>recherchePrixJournalier(vector<PrixJournalier> liste,const Date&d)
-{
-    cout<<"g1"<<endl;
+vector<PrixJournalier>recherchePrixJournalier(vector<PrixJournalier> liste,const Date&d){
     int left=0;
     int right=liste.size()-1;
     int mid=(right+left)/2;
-    cout<<"g2"<<endl;
-    if (!(liste[mid].getDate()==d)) {
-        cout<<"g3"<<endl;
-        rechercheDichotomiqueVector(d,liste,left,right,mid);
-        cout<<"g4"<<endl;
-    }
+    if (!(liste[mid].getDate()==d)) {rechercheDichotomiqueVector(d,liste,left,right,mid);}
     vector<PrixJournalier>resultat;
-    cout<<"g5"<<endl;
     //bool test=false;
-    for(int i=left;i<=right && left<=right ;i++){
-        cout<<"g6"<<endl;
-        if(d==liste[i].getDate()){
-            cout<<"g7"<<endl;
-            resultat.push_back(liste[i]);}
-        else if(d<liste[i].getDate())
-            break;
-        cout<<"g8"<<endl;
+    auto it = lower_bound(liste.begin() + left, liste.begin() + right + 1, PrixJournalier(d, "", 0));
+    while (it != liste.end() && it->getDate() == d) {
+        
+        resultat.push_back(*it);
+        if(d<it->getDate()) break;
+        it++;
+
     }
     return resultat;
 }
@@ -400,9 +376,6 @@ vector<PrixJournalier> BourseDictDate::getHistoriqueAction(string nomact)const{
 
 
 double Bourse::getLastPrixAction(string nomAct)const{
-    vector<string>p=this->getActionDisponibleAujourdhui();
-    if(appartientAction(nomAct,p))
-        return this->getPrixAujourdhui(nomAct,p);
     vector<PrixJournalier> vectHistoria=this->getHistoriqueAction(nomAct);
     return vectHistoria[vectHistoria.size()-1].getPrix();
 }
@@ -465,10 +438,9 @@ vector<PrixJournalier>BourseSet::getPrixJournaliersParDate(const Date&date)const
 
 vector<string> BourseSet::getActionDisponibleParDate(const Date& date)const{
     vector<string>v;
-        auto it=lower_bound(historique.begin(),historique.end(),PrixJournalier(date,"",0));
-        while ((it->getDate()==date)){
-            v.push_back(it->getNomAction());
-            it++;
+    vector<PrixJournalier>vpj=getPrixJournaliersParDate(date)  ;
+        for(auto elt :vpj){
+            v.push_back(elt.getNomAction());
         }
     return v;
 }
@@ -477,6 +449,7 @@ vector<PrixJournalier>BourseSet::getHistoriqueAction(string nom)const{
     for(auto it= historique.begin();it!=historique.end();++it){
         if(it->getNomAction()==nom)
             v.push_back(*it);
+        if (it->getDate()>dateAujourdhui) break; 
     }
     return v;
 
